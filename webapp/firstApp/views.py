60b8de8a781777c05040a564b50f0dd2c8df805b
Tanzeem Agra <xerxez.in@gmail.com>
2023-01-30 19:44:45 +0530

    Session 13

--- webapp/firstApp/views.py
+++ webapp/firstApp/views.py
@@ -3,7 +3,7 @@ import joblib
 import os
 import json
 import pandas as pd
-#from .models import medicine123
+from .models import medicine_exp
 import psycopg2
 
 # Create your views here.
@@ -23,8 +23,8 @@ def result(request):
 
     answer=model.predict([list]).tolist()[0]
 
-    #b=medicine123(age=request.GET['age'],sex=request.GET['sex'],bmi=request.GET['bmi'],children=request.GET['children'], smoker=request.GET['smoker'], region=request.GET['region'],charges=answer)
-    #b.save()
+    b=medicine_exp(age=request.GET['age'],sex=request.GET['sex'],bmi=request.GET['bmi'],children=request.GET['children'], smoker=request.GET['smoker'], region=request.GET['region'],charges=answer)
+    b.save()
 
     return render(request, "index.html", {'answer':answer})
 
